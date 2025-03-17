3025b9bf5e9b839f11c0a6694a8db42b87cc3bcc
Vedaant Sankhe <vedaant.sankhe@gmail.com>
2025-03-18 00:04:08 +0530

    added flash message

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import { RootStack } from './src/navigation/MainStack/RootStack'
+import FlashMessage from 'react-native-flash-message'
 
 const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
       <RootStack />
+      <FlashMessage />
     </SafeAreaView>
   )
 }
